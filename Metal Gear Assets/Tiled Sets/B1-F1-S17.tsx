622d29f2937155925d973cfac79aa143fb316b58
Frankie Lyons <c00177077@itcarlow.ie>
2017-07-02 21:24:47 +0100

    Added screens 16 and 17. Building 1, Level 1 map complete, all warps working (incremented testing). Work on adding enemies, doors, items and UI to begin.

---
+++ Metal Gear Assets/Tiled Sets/B1-F1-S17.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="b1-l1-s17" tilewidth="8" tileheight="8" tilecount="768" columns="32">
+ <image source="../Scene Images/b1-l1-s17.png" width="256" height="192"/>
+</tileset>
