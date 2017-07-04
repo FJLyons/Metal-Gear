99c722b6abe3a72895c267c888306dbc0eaae81b
Frankie Lyons <c00177077@itcarlow.ie>
2017-07-04 22:07:16 +0100

    Added base items and inventory scripts

---
+++ Metal Gear Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private List<BaseItem> m_inventory = new List<BaseItem>();
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 3; i++)
+        {
+            BaseItem m_item = new BaseItem();
+            m_inventory.Add(m_item);
+
+            Debug.Log(
+                m_inventory[i].ItemName + ": '" + 
+                m_inventory[i].ItemDescription + "' - [" +
+                m_inventory[i].ItemIndex + "] [" + 
+                m_inventory[i].ItemType + "]");
+        }
+
+        Debug.Log(m_inventory.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Metal Gear Project/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseItem {
+
+    private string m_name;
+    private string m_description;
+    private int m_index;
+
+    private ItemTypes m_type;
+
+    public BaseItem()
+    {
+        ItemName = "Item " + Random.Range(0, 11);
+        ItemDescription = ItemName + " description";
+        ItemIndex = Random.Range(0, 33);
+        ItemType = ItemTypes.CARD;
+    }
+
+
+    public enum ItemTypes
+    {
+        CONSUMABLE, // Is consumed
+        WEAPON,     // Is equipped and changes weapon 
+        WEARABLE,   // Is equipeped and has effect
+        CARD,       // Used for doors
+        EXPLOSIVE   // Is equipped and planted
+    }
+
+    public string ItemName
+    {
+        get { return m_name; }
+        set { m_name = value; }
+    }
+
+    public string ItemDescription
+    {
+        get { return m_description; }
+        set { m_description = value; }
+    }
+
+    public int ItemIndex
+    {
+        get { return m_index; }
+        set { m_index = value; }
+    }
+
+    public ItemTypes ItemType
+    {
+        get { return m_type; }
+        set { m_type = value; }
+    }
+}
